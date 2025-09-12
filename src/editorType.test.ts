76efcef2d6b3ea7455b868e75611185e7981c7eb
Alex Vernacchia <vernak2539@users.noreply.github.com>
2025-09-12 07:28:06 -0400

    feat(editorType): add editorType functionality

---
+++ src/editorType.test.ts
@@ -0,0 +1,79 @@
+import { expect, test, describe, beforeEach, afterEach } from "bun:test";
+import { window as global } from "../test/utils/window";
+import * as figmaEditorType from "./editorType";
+
+global.figma = {
+  editorType: "figma",
+} as any;
+
+global.figma.editorType = "figma";
+
+beforeEach(() => {
+  global.figma = {} as any;
+});
+
+afterEach(() => {
+  window.figma = null;
+});
+
+test("getCurrentEditorType", () => {
+  global.figma.editorType = "figma";
+
+  const editorType = figmaEditorType.getCurrentEditorType();
+
+  expect(editorType).toBe("figma");
+});
+
+describe("isFigmaEditor", () => {
+  test("should return true if figma editor", () => {
+    global.figma.editorType = "figma";
+
+    const actual = figmaEditorType.isFigmaEditor();
+
+    expect(actual).toBe(true);
+  });
+
+  test("should return false if the editor is not figma", () => {
+    global.figma.editorType = "dev";
+
+    const actual = figmaEditorType.isFigmaEditor();
+
+    expect(actual).toBe(false);
+  });
+});
+
+describe("isDevEditor", () => {
+  test("should return true if dev editor", () => {
+    global.figma.editorType = "dev";
+
+    const actual = figmaEditorType.isDevEditor();
+
+    expect(actual).toBe(true);
+  });
+
+  test("should return false if the editor is not dev", () => {
+    global.figma.editorType = "figma";
+
+    const actual = figmaEditorType.isDevEditor();
+
+    expect(actual).toBe(false);
+  });
+});
+
+describe("isFigjamEditor", () => {
+  test("should return true if figjam editor", () => {
+    global.figma.editorType = "figjam";
+
+    const actual = figmaEditorType.isFigjamEditor();
+
+    expect(actual).toBe(true);
+  });
+
+  test("should return false if the editor is not figjam", () => {
+    global.figma.editorType = "figma";
+
+    const actual = figmaEditorType.isFigjamEditor();
+
+    expect(actual).toBe(false);
+  });
+});
---
+++ src/editorType.ts
@@ -0,0 +1,17 @@
+// File contains functionality relating to the figma plugin mode
+
+export const getCurrentEditorType = (): PluginAPI["editorType"] => {
+  return figma.editorType;
+};
+
+export const isFigmaEditor = (): boolean => {
+  return getCurrentEditorType() === "figma";
+};
+
+export const isDevEditor = (): boolean => {
+  return getCurrentEditorType() === "dev";
+};
+
+export const isFigjamEditor = (): boolean => {
+  return getCurrentEditorType() === "figjam";
+};
